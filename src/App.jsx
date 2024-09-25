43b2968e4eaeed6094ea8d7f297ea1978b635e61
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-26 01:36:03 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import SingleBlog from "./pages/SingleBlog"
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:id" element={<SingleBlog />} />
+      
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
