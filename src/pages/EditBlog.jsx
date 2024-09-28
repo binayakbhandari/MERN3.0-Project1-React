d0b6d6d55d496712da252bcb46f54d3b11afffd7
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-28 18:11:26 +0545

    error fixed

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -56,41 +56,7 @@ function EditBlog({ type }) {
         <>
             <Navbar />
             {/* <Form type="Edit" id={id} /> */}
-            <div className="max-w-4xl mx-auto font-[sans-serif] p-6">
-                <div className="text-center mb-16">
-                    <Link to="">
-                        <img src="https://avatars.githubusercontent.com/u/173297872?v=4" alt="logo" className='w-24 h-24 mb-3 rounded-full shadow-lg inline-block' />
-                    </Link>
-                    <h4 className="text-gray-800 text-base font-semibold mt-6">Edit Your Own Blog</h4>
-                </div>
-
-                <form onSubmit={editBlog}>
-                    <div className="grid sm:grid-cols-1 gap-8">
-                        <div>
-                            <label className="text-gray-800 text-sm mb-2 block">Title</label>
-                            <input name="title" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter title" onChange={handleChange} value={data.title} />
-                        </div>
-                        <div>
-                            <label className="text-gray-800 text-sm mb-2 block">Subtitle</label>
-                            <input name="subtitle" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter subtitle" onChange={handleChange} value={data.subtitle} />
-                        </div>
-                        <div>
-                            <label className="text-gray-800 text-sm mb-2 block">Image</label>
-                            <input name="image" type="file" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all" onChange={handleChange}  />
-                        </div>
-                        <div>
-                            <label className="text-gray-800 text-sm mb-2 block">Description</label>
-                            <textarea name="description" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter description" onChange={handleChange} value={data.description} />
-                        </div>
-                    </div>
-
-                    <div className="!mt-12">
-                        <button type="submit" className="py-3.5 px-7 text-sm font-semibold tracking-wider rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
-                            Edit Blog
-                        </button>
-                    </div>
-                </form>
-            </div>
+            
         </>
     )
 }
